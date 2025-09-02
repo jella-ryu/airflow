e5afa6c10179018346f9082d473f087dbc0651cc
jella-ryu <minseung.ryou@gmail.com>
2025-09-02 12:06:41 +0900

    파일 경로 수정

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -6,7 +6,7 @@ from airflow.operators.python import PythonOperator
 from common.common_func import get_sftp
 
 with DAG(
-    dag_id="dags_python_operator", # 화면에서 보이는 값인데, 파일명과 일치시키는게 좋음.
+    dag_id="dags_python_import_func", # 화면에서 보이는 값인데, 파일명과 일치시키는게 좋음.
     schedule="30 6 * * *", # 크론 스케줄
     start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"), # 덱이 언제부터 돌건지, 한국시간으로
     catchup=False, # 덱이 처음 돌때 과거 데이터를 처리할지 여부(true는 상황에 따라 위험. 한번에 돈다)
