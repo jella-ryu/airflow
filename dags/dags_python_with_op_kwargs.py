9ad7297cca8abfe0cfbacb3c43b6733fddd8c0a5
jella-ryu <minseung.ryou@gmail.com>
2025-09-03 11:23:45 +0900

    dags with op kwargs upload

---
+++ dags/dags_python_with_op_kwargs.py
@@ -0,0 +1,21 @@
+import pendulum
+from common.common_func import regist
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    regist2_t1 = PythonOperator(
+        task_id='regist2_t1',
+        python_callable=regist2,
+        op_args=['hjkim','man','kr','seoul'],
+        op_kwargs={'email':'hjkim@gmail.com','phone':'010-1234-5678'}
+    )
+
+    regist2_t1
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -5,3 +5,16 @@ def regist(name, sex, *args):
     print(f'이름: {name})')
     print(f'성별: {sex}')
     print(f'기타 정보들: {args}')
+
+def regist2(name, sex, *args, **kwargs):
+    print(f'이름: {name}')
+    print(f'성별: {sex}')
+    print(f'기타 정보들: {args}')
+    # 값이 있는지 확인
+    email = kwargs['email'] or None
+    phone = kwargs['phone'] or None
+    # 값이 있으면 출력
+    if email:
+        print(email)
+    if phone:
+        print(phone)
